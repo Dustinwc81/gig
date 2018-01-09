78a5d2bce8f8a1769a751f515611528449cf7096
DCrowder <crowderdwcm@gmail.com>
2018-01-08 21:09:18 -0600

    seeder

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -14,10 +14,26 @@ class ProductsTableSeeder extends Seeder
     {
         Products::create([
         'name' => 'JH2 Shirt',
-        'slug' => 'shirt',
+        'slug' => 'black shirt',
         'details' => 'Black, Green JH2 Emblem, Cotton',
         'price' => 1499,
         'description' => 'Lorem Ipsum',
      ]);
+
+        Products::create([
+        'name' => 'JH2 Shirt White',
+        'slug' => 'white shirt',
+        'details' => 'White, Green JH2 Emblem, Cotton',
+        'price' => 1499,
+        'description' => 'Lorem Ipsum',
+    ]);
+
+        Products::create([
+       'name' => 'JH2 Shirt Green',
+       'slug' => 'greeen shirt',
+       'details' => 'Green, Green JH2 Emblem, Cotton',
+       'price' => 1499,
+       'description' => 'Lorem Ipsum',
+      ]);
     }
 }
