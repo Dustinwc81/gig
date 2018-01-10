e140ad085fb9b15ea104c307e3f396bfc5645a45
Christian Chapman <cdchap@protonmail.com>
2018-01-10 11:36:24 -0600

    added relationships in models for threads/replies/owners thread->belongsTO(user)->hasMany(reply)...etc

--- app/Thread.php
+++ app/Thread.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    //
+    protected function creator()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    protected function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
