0f746e0312df6cbbec65951c3afc3a531c26d441
Christian Chapman <cdchap@protonmail.com>
2018-01-10 13:13:45 -0600

    made basic cards for threads on the forum index page

--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -4,13 +4,26 @@
 
 @section('content')
 
-    @foreach ($threads as $thread)
-        <div>
-            {{ $thread->body }}
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <div class="border border-white margin-bottom-20">
+                <h1 class="text-white">FORUM</h1>
+            </div>
         </div>
-    @endforeach
-    <div>
-        What the shit?
     </div>
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            @foreach ($threads as $thread)
+                    <div class="card margin-ten">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                {{ $thread->title }}
+                            </h5>
+                            <p class="card-text text-left">{{ substr(strip_tags($thread->body), 0, 150)}}{{ strlen(strip_tags($thread->body)) > 150 ? "&#8230;" : "" }} <a class="card-link" href="">LINK</a></p>
 
+                        </div>
+                    </div>
+            @endforeach
+        </div>
+    </div>
 @stop
