5fde1b87551dcc7e8bc292e5b9adcab89dc34418
DCrowder <crowderdwcm@gmail.com>
2018-01-16 14:57:23 -0600

    migrated youthtournament results table

--- database/migrations/2018_01_16_144343_create_youthtournamentresults_table.php
+++ database/migrations/2018_01_16_144343_create_youthtournamentresults_table.php
@@ -15,6 +15,31 @@ class CreateYouthtournamentresultsTable extends Migration
     {
         Schema::create('youthtournamentresults', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('ageGroup');
+            $table->year('year');
+            $table->string('firstPlace');
+            $table->string('firstPlacePoints');
+            $table->string('secondPlace');
+            $table->string('secondPlacePoints');
+            $table->string('thirdPlace');
+            $table->string('thirdPlacePoints');
+            $table->string('fourthPlace');
+            $table->string('fourthPlacePoints');
+            $table->string('fifthPlace');
+            $table->string('fifthPlacePoints');
+            $table->string('sixthPlace');
+            $table->string('sixthPlacePoints');
+            $table->string('seventhPlace');
+            $table->string('seventhPlacePoints');
+            $table->string('eighthPlace');
+            $table->string('eighthPlacePoints');
+            $table->string('ninthPlace');
+            $table->string('ninthPlacePoints');
+            $table->string('tenthPlace');
+            $table->string('tenthPlacePoints');
+            $table->string('bigFish');
+            $table->string('bigFishPoints');
+            $table->text('notes');
             $table->timestamps();
         });
     }
