ed40ba9285b5d8082e5e83de8402961d3845aa1c
Christian Chapman <cdchap@protonmail.com>
2018-01-19 09:57:49 -0600

    added the gigs to the hero section

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -9,7 +9,7 @@
         <div class="row">
             <div class="hero">
                <div class="hero-image">
-                   <img src="{{ URL::asset('images/water-600.jpg') }}" alt="water" class="water-image">
+                   <img src="{{ URL::asset('images/IMG_3757.jpg') }}" alt="water" class="water-image">
                </div>
                 <h1 class="hero-title">JH2 GIGS</h1>
             </div>
@@ -21,40 +21,42 @@
                 <h1 class="text-white top-spacer-80 spacer">Gig Gear</h1>
             </div>
         </div>
-        <div class="row top-spacer-60">
-            <div class="col-sm-6">
-                <div class="row">
-                    <div class="col">
-                        <div class="card gig-merch-container">
-                            <img class="card-img " src="{{ URL::asset('images/three-prong-200.jpg')}}" alt="Three Prong">
-                            <div class="card-img-overlay" id="card1">
+        <div class="products-container">
+            <div class="row top-spacer-60">
+                <div class="col-sm-6">
+                    <div class="row">
+                        <div class="col">
+                            <div class="card gig-merch-container">
+                                <img class="card-img image-fill" src="{{ URL::asset('images/IMG_1558.JPG')}}" alt="Three Prong">
+                                <div class="card-img-overlay" id="card1">
 
-                             </div>
+                                 </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card gig-merch-container">
+                                <img class="card-img" src="{{ URL::asset('images/three-prong-200.jpg')}}" alt="Card image">
+                            </div>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="card gig-merch-container">
-                            <img class="card-img" src="{{ URL::asset('images/three-prong-200.jpg')}}" alt="Card image">
+                    <div class="row">
+                        <div class="col">
+                            <div class="card gig-merch-container">
+                                <img class="card-img" src="{{ URL::asset('images/USA_Oklahoma_location_map.svg')}}" alt="Card image">
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col top-spacer-60">
-                        <div class="card gig-merch-container">
-                            <img class="card-img" src="{{ URL::asset('images/USA_Oklahoma_location_map.svg')}}" alt="Card image">
-                        </div>
+                <div class="col-sm-6">
+                    <div class="card gig-merch-container">
+                        <img class="card-img large-merch-container" src="{{ URL::asset('images/hat-600.jpg')}}" alt="hat">
+                        <div class="card-img-overlay" id="">
+
+                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-6">
-                <div class="card gig-merch-container">
-                    <img class="card-img large-merch-container" src="{{ URL::asset('images/hat-600.jpg')}}" alt="hat">
-                    <div class="card-img-overlay" id="">
+        </div>
 
-                     </div>
-                </div>
-            </div>
-         </div>
-      </div>
     <script src="{{ URL::asset("js/welcome.js")}}"></script>
 @endsection
