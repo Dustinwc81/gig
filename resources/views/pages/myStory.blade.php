c656957156642d51dce977c78b97a943c1481950
DCrowder <crowderdwcm@gmail.com>
2018-01-20 09:32:15 -0600

    changed my story

--- resources/views/pages/myStory.blade.php
+++ resources/views/pages/myStory.blade.php
@@ -7,7 +7,7 @@
          <div class="row">
             <div class="col-md-12">
                <h1 class="text-white">MY STORY</h1>
-               <p class="text-white">I'm the champion bitches</p>
+               <p class="text-white">Coming Soon!!</p>
             </div>
          </div>
 
