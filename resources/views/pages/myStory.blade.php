544f5667c31d2c50f44f72a8a46af62a28205a70
DCrowder <crowderdwcm@gmail.com>
2018-01-21 13:36:39 -0600

    test

--- resources/views/pages/myStory.blade.php
+++ resources/views/pages/myStory.blade.php
@@ -7,7 +7,7 @@
          <div class="row">
             <div class="col-md-12">
                <h1 class="text-white">MY STORY</h1>
-               <p class="text-white">Coming Soon!!</p>
+               <p class="text-white">Please be patient, Coming Soon!</p>
             </div>
          </div>
 
