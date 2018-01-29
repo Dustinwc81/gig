d3c0bfacc4d56dbe5119b81bd43f59f7afa5a28c
Christian Chapman <cdchap@protonmail.com>
2018-01-29 12:41:00 -0600

    changed the link color from blue to light

--- resources/views/forum/reply.blade.php
+++ resources/views/forum/reply.blade.php
@@ -1,6 +1,6 @@
 <div class="card margin-ten-around border border-secondary">
     <div class="card-header bg-secondary text-light">
-        <a href="#" class="text-primary">
+        <a href="#" class="text-light">
             {{ $reply->owner->name }}
         </a> said {{ $reply->created_at->diffForHumans() }}&#8230;
     </div>
