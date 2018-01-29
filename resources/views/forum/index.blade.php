9c5b9a7ddc0cafc491c608e59dbc6e61faf55151
Christian Chapman <cdchap@protonmail.com>
2018-01-29 12:37:40 -0600

    chanced the look slightly of the individual forum pagees

--- resources/views/forum/index.blade.php
+++ resources/views/forum/index.blade.php
@@ -14,16 +14,16 @@
     <div class="row">
         <div class="col-md-8 offset-md-2">
             @foreach ($threads as $thread)
-                    <div class="card margin-ten">
+                    <div class="card margin-ten-around">
                         <div class="card-header">
                             <h5 class="card-title">
                                 {{ $thread->title }}
                             </h5>
                         </div>
-                        <div class="card-body">
+                        <div class="card-body bg-dark text-light">
                             <p class="card-text text-left">{{ substr(strip_tags($thread->body), 0, 150)}}{{ strlen(strip_tags($thread->body)) > 150 ? "&#8230;" : "" }} <a class="card-link" href="{{ $thread->path() }}">read more</a></p>
                         </div>
-                        <div class="card-footer text-muted">
+                        <div class="card-footer text-light">
                             {{ $thread->creator->name }}
                         </div>
                     </div>
