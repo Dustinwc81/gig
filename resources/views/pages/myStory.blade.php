877d1745fb60cf302ee9b2f9ab16c16a37d024f7
DCrowder <crowderdwcm@gmail.com>
2018-02-04 22:03:49 -0600

    misspelling in mystory

--- resources/views/pages/myStory.blade.php
+++ resources/views/pages/myStory.blade.php
@@ -8,13 +8,45 @@
          <div class="row">
             <div class="col-sm-10 offset-sm-1">
                <h1 class="text-white spacer"><strong>JH2's Story</strong></h1>
-               <p class="text-white text-left">Back in 1998 I had lost one of my best gigs in Elk River. It broke off the handle and we could not find it after looking for almost an hour.  I had several other gigs that I inherited from my dad but they were pretty worn out from many years of use and in need of some repair.  Most of them were identified by the name of the people that had made them: Scroggins, Sissom, Webb, Botts. These were common names if you grew up around Delaware County in Northeastern Oklahoma and spent time gigging at Lake Eucha, Low Water, or Drowning Creek.  However, when I tried to go and buy a gig, I found that it was easier said than done.  Clarence Scroggins and Bill Sissom quit making gigs in the late 80’s-early 90’s, Jesse Webb still made a few of them but he was busy with other jobs and only made them for a short time each year, and Tilford Botts lived in Colcord and I had no idea how to find him.  I loved gigging, I had been going since I could walk and went every chance I got. I couldn’t imagine not being able to go because I didn’t have a good gig.<br>
+               <p class="text-white text-left">Back in 1998 I had lost one of my best gigs in Elk River. It broke off
+                  the handle and we could not find it after looking for almost an hour.  I had several other gigs that
+                  I inherited from my dad but they were pretty worn out from many years of use and in need of some
+                  repair.  Most of them were identified by the name of the people that had made them: Scroggins, Sissom,
+                  Webb, Botts. These were common names if you grew up around Delaware County in Northeastern Oklahoma
+                  and spent time gigging at Lake Eucha, Low Water, or Drowning Creek.  However, when I tried to go and
+                  buy a gig, I found that it was easier said than done.  Clarence Scroggins and Bill Sissom quit making
+                  gigs in the late 80’s-early 90’s, Jesse Webb still made a few of them but he was busy with other jobs
+                  and only made them for a short time each year, and Tilford Botts lived in Colcord and I had no idea
+                  how to find him.  I loved gigging, I had been going since I could walk and went every chance I got.
+                  I couldn’t imagine not being able to go because I didn’t have a good gig.<br>
                <br>
-               When I was in my early 20's I had helped my cousin build a couple gigs that ended up winning the <a class="text-secondary" href="/tournament"><u>National Gigging Tournament</u></a> for us.  I started thinking about how awesome it would be if I could build a gig myself.  I had some welding experience so I created a gig design I liked.  However, I had no idea where to begin on making the ferrule and attaching it to the gig.  I knew there was basically one person I could talk to who could tell me everything I needed to know, so I drove to Jesse Webb's house and luckily he was home.  I knew who he was and he knew who I was because of my dad but I had never really actually spoken to him.  Jesse was very nice, he invited me into his shop and began answering every question I had about making gigs and tempering them.  I left that night confident I could make a good gig!<br>
+               When I was in my early 20's I had helped my cousin build a couple gigs that ended up winning the
+                  <a class="text-secondary" href="/tournament"><u>National Gigging Tournament</u></a> for us.  I started
+                  thinking about how awesome it would be if I could build a gig myself.  I had some welding experience
+                  so I created a gig design I liked.  However, I had no idea where to begin on making the ferrule and
+                  attaching it to the gig.  I knew there was basically one person I could talk to who could tell me
+                  everything I needed to know, so I drove to Jesse Webb's house and luckily he was home.  I knew who he
+                  was and he knew who I was because of my dad but I had never really actually spoken to him.  Jesse was
+                  very nice, he invited me into his shop and began answering every question I had about making gigs and
+                  tempering them.  I left that night confident I could make a good gig!<br>
                <br>
-               Over the next couple of years I made about 25 gigs out of about any type of spring steel I could find.  My earliest gigs were made out old plow points. They were really good gigs but they took a while to make because I had to cut and grind the outsides of the gig also.  Nowadays I make them all out of 5160 Spring Steel. <a class="text-secondary" href="/facts"><u>Greg Wilson</u></a> owns most of the first gigs I made. If anyone gets any credit for helping my gig making career take off it would have to go to him. He was one of the first people to buy my gigs and tell other people about them.  Greh and his brother Steve Wilson won 4 consecutive National Gigging Titles with my gigs in the early 2000's.  That is when people began to notice more and more that my gigs held fish well and were built to last.  <br>
+               Over the next couple of years I made about 25 gigs out of about any type of spring steel I could find.
+                  My earliest gigs were made out old plow points. They were really good gigs but they took a while to
+                  make because I had to cut and grind the outsides of the gig also.  Nowadays I make them all out of
+                  5160 Spring Steel. <a class="text-secondary" href="/facts"><u>Greg Wilson</u></a> owns most of the
+                  first gigs I made. If anyone gets any credit for helping my gig making career take off it would have
+                  to go to him. He was one of the first people to buy my gigs and tell other people about them.  Greg
+                  and his brother <a class="text-secondary" href="/facts"><u>Steve Wilson</u></a> won 4 consecutive
+                  National Gigging Titles with my gigs in the early 2000's. That is when people began to notice more and
+                  more that my gigs held fish well and were built to last.  <br>
                <br>
-               Since 1998 I have made over 500 gigs in many different sizes and shapes, from 1” crawdad gigs to 5 ¼” Spoonbill/Carp gigs and everything in between.  I’m sure there are several lost to the creeks and lakes in which they are used but almost all of them are still in use today.  I still make them all by hand in the same shop almost the same way I did in the beginning. The only difference is I use a new welder and I’ve learned a few things that save a little bit of time when making them.  Each gig takes about 2 hours to make and I take great pride in building each one.  I think if you take the time to pick one up you will be quite happy you did.<br>
+               Since 1998 I have made over 500 gigs in many different sizes and shapes, from 1” crawdad gigs to
+                  5 ¼” Spoonbill/Carp gigs and everything in between.  I’m sure there are several lost to the creeks and
+                  lakes in which they are used but almost all of them are still in use today.  I still make them all by
+                  hand in the same shop almost the same way I did in the beginning. The only difference is I use a new
+                  welder and I’ve learned a few things that save a little bit of time when making them.  Each gig takes
+                  about 2 hours to make and I take great pride in building each one.  I think if you take the time to
+                  pick one up you will be quite happy you did.<br>
                <br>
 
                <a href="/gigs" class="text-success">John Henry Ward II <br>
