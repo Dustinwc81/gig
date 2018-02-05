fab0b71fde95ad837c1c6edc195dd607f9885c7c
DCrowder <crowderdwcm@gmail.com>
2018-02-04 20:43:44 -0600

    Added Gigging Tournament Route, and other minor changes

---
+++ resources/views/pages/giggingTournament.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('title', 'Gigging Tournament')
+
+@section('description', 'National Gigging Tournament History, Location, Dates, Times.  Lake Eucha in Jay, Oklahoma')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-md-12 text-white">
+            <p>COMING SOON!!</p>
+        </div>
+    </div>
+
+@endsection
