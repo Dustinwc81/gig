1e989cef2e461383c45c8b0490cae62409b1c657
Christian Chapman <cdchap@protonmail.com>
2018-03-06 18:46:28 -0600

    chnged the source of imgaes to the smaller ones

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -10,7 +10,7 @@
         <div class="row">
             <div class="hero">
                <div class="hero-image">
-                   <img src="{{ URL::asset('images/IMG_3757.jpg') }}" alt="Gigs" class="water-image">
+                   <img src="{{ URL::asset('images/multi.jpg') }}" alt="Gigs" class="water-image">
                </div>
                 <h1 class="hero-title">JH2 GIGS</h1>
             </div>
@@ -29,7 +29,7 @@
                         <div class="col">
                             <div class="card">
                                 <a href="{{ route('shop.index')}}"><img class="img-fluid" src="{{ URL::asset
-                                ('images/IMG_1558.JPG')}}" alt="gigs"></a>
+                                ('images/many.JPG')}}" alt="gigs"></a>
                             </div>
                         </div>
                     </div>
@@ -48,7 +48,7 @@
                         ('images/IMG_2073.JPG')}}" alt="gigs"></a>
                     </div>
                     <div class="center top-spacer-80">
-                        
+
                     </div>
                 </div>
             </div>
