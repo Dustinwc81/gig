12b0c622720bf8ab2cad0e3a5895782462fc8453
Christian Chapman <cdchap@protonmail.com>
2018-04-03 10:17:18 -0500

    styled tourney page

--- resources/views/pages/giggingTournament.blade.php
+++ resources/views/pages/giggingTournament.blade.php
@@ -18,22 +18,27 @@
             <div class="top-spacer-20"></div>
         </div>
     </div>
-
-    <div class="tournament-story top-spacer-20">
+    <div class="row">
+        <div class="title-container">
+            <h1 class="tournament-text1">
+                Old Charter Whiskey and Some Fried Fish
+            </h1>
+        </div>
+    </div>
+    <div class="tournament-story top-spacer-20 padding-bottom-60">
         <div class="row">
-            <div class="col-md-10 offset-1 text-left text-white">
-                <h1 class="tournament-text1">"Old Charter Whiskey and Some Fried Fish"</h1>
+            <div class="col-md-8 offset-2 text-left text-white">
                 <p>
-                    <span class="tournament-text1"></span>  That was how the
-                    gigging tournament
+                    That was how the gigging tournament
                     was born.  Back in
                     April of 1972, John Ward and Matt Mathews went gigging on Spavinaw Creek near Low Water bridge
                     outside Jay, Oklahoma.  They had killed enough fish to eat and were sitting around eating what they
                     had caught and enjoying some Old Charter when John Ward suggested having a gigging tournament like
                     bass fishermen had.  They originally thought about having it on two lakes (Lake Eucha & Spavinaw)
                     on alternating nights, but soon realized that would be too difficult.  So they eventually settled
-                    on having it at Lake Eucha.<br>
-                    <br>
+                    on having it at Lake Eucha.
+                </p>
+                <p>
                     They began organizing the tournament and planned on having it the month of April because that’s when
                     fish were the most plentiful in the creeks..  The early tournaments did not have a preset date but
                     they would choose the date by avoiding Easter and the full moon.  Today’s tournaments are held on the
@@ -42,9 +47,19 @@
                     per pound for Suckers, 3 points per pound for White Bass, and 1 point per pound for Carp, Perch,
                     Flathead, etc.  It is the same scoring format that is still used today.  Throughout the years they
                     gave fun prizes like smallest fish and the attaboy award for 11th place, but these changed slightly
-                    each year.  They also had a Golden Gig award for people who went above and beyond helping with the
-                    tournament.<br>
-                    <br>
+                    each year.  They also had a Golden Gig award for people who went above
+                </p>
+
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 offset-2 quote-container center">
+                <p class="quote">&ldquo;It was a little too crazy&rdquo;</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 offset-2 text-left text-white">
+                <p>
                     They consulted with Joe Hutchison who was the local Game Warden to make sure everything they were
                     doing was legal.  John and Matt funded the tournament themselves for the first two years.  They sold
                     t-shirts, jackets, patches, and ran a concession stand at the tournament to help raise money.  As it
@@ -53,22 +68,24 @@
                     not great for gigging.  The take off was scheduled for 9:00 and at 7:15 only about 6 teams were signed
                     up and ready to go.  John and Matt were talking about how they had really wasted their time and effort,
                     when trucks and boats started showing up and by 9 o’clock 47 teams had shown up to be a part of the 1st
-                    gigging tournament.<br>
-                    <br>
+                    gigging tournament.
+                </p>
+                <p>
                     John and Matt wanted to have a public fish fry on the second day of the tournament, they used the
                     fish that was gigged on the first night of the tournament and 100 lbs of beans to feed everyone.
                     Both soon learned that they did not have near enough people cooking and needed more food to feed all
-                    the people that showed up. The fish fry for the public is a tradition that is still continued today.<br>
-                    <br>
+                    the people that showed up. The fish fry for the public is a tradition that is still continued today.
+                </p>
+                <p>
                     Some of the early rules made the tournament fun to watch.  The take-off was done by drawing a number
                     and getting all boats lined up against the bank with one man at the motor which was not allowed to
                     be running and the other man on the bank ready to push off.  A flare was shot and everyone had to
-                    start their motor and the race was on.  <span class="tournament-text1"> IT WAS A LITTLE TOO CRAZY...
-                     </span> considering there were no motor
+                    start their motor and the race was on. IT WAS A LITTLE TOO CRAZY...considering there were no motor
                     size restrictions and people were going all directions.  Eventually they moved to declaring which
                     direction the team was going to go and motors were limited to 15 hp.  This greatly improved the
-                    take-off but it was still fun to watch for the occasional boat wreck and busted lights.<br>
-                    <br>
+                    take-off but it was still fun to watch for the occasional boat wreck and busted lights.
+                </p>
+                <p>
                     One of the biggest hurdles the early tournaments had to clear was the getting the permission to use
                     Lake Eucha and lobbying against a gigging season in Delaware County.  Lake Eucha is owned by the City
                     of Tulsa, therefore every year John Ward and then eventually the NGCGA had to request the use of Lake
@@ -79,11 +96,22 @@
                     to implement a gigging season.  We have one now that is currently in effect for the entire state from
                     December 1st to March 31st.  During this time, John Ward and several other NGCGA members went to the
                     State Capitol and lobbied against a statewide gigging season.  Their efforts were rewarded,
-                    <span class="tournament-text"> in Delaware
-                    and Mayes counties gigging is now allowed all year long.</span>  One of the biggest reasons was because of the
+                    in Delaware
+                    and Mayes counties gigging is now allowed all year long. One of the biggest reasons was because of the
                     large percentage of Cherokee and other Native Americans that rely on gigging to keep family and tribal
-                    traditions alive.<br>
-                    <br>
+                    traditions alive.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 offset-2 quote-container center">
+                <p class="quote">&ldquo;in Delaware
+                and Mayes counties gigging is now allowed all year long&rdquo;</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 offset-2 text-left text-white">
+                <p>
                     The National Green Country Gigging Tournament is getting ready for its 46th year.  It would not be
                     possible if it weren’t for the hard work and dedication of so many over the years that have kept the
                     vision of John Ward and Matt Mathews alive.  It’s fun to look back at the old gigging tournaments and
@@ -96,20 +124,17 @@
                     the early years.  After that came 12 volt lights, then the biggest change was aided by electric
                     generators.  Boats were able to go to halogen lights, mercury vapors, high pressure sodium, and metal
                     halides.  Today most boats use LED lights that are extremely bright and use less wattage than other
-                    lights.<br>
-                    <br>
+                    lights.
+                </p>
+                <p>
                     The one thing that has remained constant is the people.  People still show up by the thousands to watch
                     take-off and weigh-in every year.  In the beginning most people stayed in tents and a select few had
                     campers.  Today there are a few tents but mostly it is campers and RV’s as far as the eye can see.
                     Most people getting there a week or two early so nobody else will get their favorite camping spot.
                     Some people are so committed they set their yearly vacation time around the gigging tournament and
-                    others have even exchanged their wedding vows there.  <span class="tournament-text1">"Not everyone who comes is a
-                        “Gigger”, but
-                    hopefully
-                    all who come leave with an fun experience that they will never forget!"</span>  That is why it all
-                    began, and it
-                    is a big part of the reason it is still continuing today.<br>
-
+                    others have even exchanged their wedding vows there.Not everyone who comes is a &ldquo;Gigger&rdquo;, but
+                    hopefully all who come leave with an fun experience that they will never forget!That is why it all
+                    began, and it is a big part of the reason it is still continuing today.
                 </p>
             </div>
         </div>
